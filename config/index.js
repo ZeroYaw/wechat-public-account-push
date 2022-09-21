15e5f041325e23bddf15a86687578ee7d63f2540
ZeroYaw <34635568+ZeroYaw@users.noreply.github.com>
2022-09-21 14:35:43 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -64,7 +64,7 @@ export const config = {
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: "op48-5mO-J9LRBLrqZOkHLAKaXwI",
       // 你想对他发送的模板消息的模板ID
-      useTemplateId: "	_4wXUGzi-abv0E5N6DPWUdT7Q-GvfHb7kfFowcswol0",
+      useTemplateId: "_4wXUGzi-abv0E5N6DPWUdT7Q-GvfHb7kfFowcswol0",
       // 所在省份
       province: "浙江",
       // 所在城市
